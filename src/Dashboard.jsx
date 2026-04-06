4782b3680a16b79b41114d9c9a9199581ffdf596
BobbyTarantino76 <matijazuber273@gmail.com>
2026-04-06 19:51:07 +0200

    created the api call to the supabase database, styled the web

---
+++ src/Dashboard.jsx
@@ -0,0 +1,40 @@
+import supabase from "./supabase-client"
+import { useEffect } from "react"
+
+function Dashboard() {
+
+const fetchMetrics = async()=>{
+  try{
+  const response = await supabase
+  .from('sales_deals')
+  .select(
+    `
+    name,
+    value
+    `,
+  )
+  .order('value', { ascending: false })
+  .limit(1)
+  
+  console.log(response)
+} catch(error){
+  console.error("Something went wrong")
+}
+
+}
+
+ useEffect(()=>{
+    fetchMetrics()
+  },[])
+  
+
+  return (
+    <div className="dashboard-wrapper">
+      <div className="chart-container">
+        <h2>Total Sales This Quarter ($)</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
