551b4c0b4c6f4aae7d4079ca000b72f18f3c9d72
BobbyTarantino76 <matijazuber273@gmail.com>
2026-04-06 21:21:11 +0200

    added the react chart and also the data from the supabase

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,45 +1,92 @@
 import supabase from "./supabase-client"
-import { useEffect } from "react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { Chart } from "react-charts"
 
 function Dashboard() {
-const [metrics,setMetrics] = useState([]);
+  const [metrics, setMetrics] = useState([])
 
-const fetchMetrics = async()=>{
-  try{
-  const {data,error} = await supabase
-  .from('sales_deals')
-  .select(
-    `
-    name,
-    total:value.sum()
-    `,
-  )
-  if(error){
-    throw error
+  const fetchMetrics = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("sales_deals")
+        .select(`
+          name,
+          value.sum()
+        `)
+
+      if (error) throw error
+
+      setMetrics(data)
+      console.log(data)
+
+    } catch (error) {
+      console.error("Something went wrong", error)
+    }
   }
-  setMetrics(data)
-  console.log(data)
-  
 
-} catch(error){
-  console.error('Something went wrong',error)
-}
-
-}
-
- useEffect(()=>{
+  useEffect(() => {
     fetchMetrics()
-  },[])
-  
+  }, [])
+
+  const primaryAxis = {
+    getValue: (d) => d.primary,
+    scaleType: "band",
+    padding: 0.2,
+    position: "bottom",
+  }
+
+  const chartData = [
+    {
+      data: metrics.map((m) => ({
+        primary: m.name,
+        secondary: m.sum, 
+      })),
+    },
+  ]
+
+  function y_max() {
+    if (metrics.length > 0) {
+      const maxSum = Math.max(...metrics.map((m) => m.sum))
+      return maxSum + 2000
+    }
+    return 5000
+  }
+
+  const secondaryAxes = [
+    {
+      getValue: (d) => d.secondary,
+      scaleType: "linear",
+      min: 0,
+      max: y_max(),
+      padding: {
+        top: 20,
+        bottom: 40,
+      },
+    },
+  ]
 
   return (
     <div className="dashboard-wrapper">
       <div className="chart-container">
         <h2>Total Sales This Quarter ($)</h2>
       </div>
+
+      <div style={{ height:'10000px' }}>
+        <Chart
+          options={{
+            data: chartData,
+            primaryAxis,
+            secondaryAxes,
+            type: "bar",
+            defaultColors: ["#58d675"],
+            tooltip: {
+              show: false,
+            },
+          }}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default Dashboard;
+export default Dashboard
